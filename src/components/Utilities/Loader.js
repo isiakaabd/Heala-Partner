7ab87387237cc852f42cbe9fab8fd1957203699a
Remlad <isiakaabd@gmail.com>
2022-07-17 18:51:04 +0100

    added

--- src/components/Utilities/Loader.js
+++ src/components/Utilities/Loader.js
@@ -1,24 +1,13 @@
 import React from "react";
-import { Player } from "@lottiefiles/react-lottie-player";
-import { Grid } from "@mui/material";
+import { Grid, CircularProgress } from "@mui/material";
 import PropTypes from "prop-types";
 const Loader = ({ color, size, ...rest }) => {
   return (
-    <Grid
-      item
-      sx={{
-        height: "80vh",
-        overflow: "hidden",
-      }}
-      container
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Player
-        loop
-        autoplay
-        style={{ height: "100%", width: "100%" }}
-        src="https://assets5.lottiefiles.com/private_files/lf30_f4qcnthw.json"
+    <Grid container justifyContent="center" alignItems="center">
+      <CircularProgress
+        size={size ? size : 20}
+        color={color ? color : "primary"}
+        {...rest}
       />
     </Grid>
   );
